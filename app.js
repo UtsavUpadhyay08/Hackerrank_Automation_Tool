8d19f94dea374f7cb37d8330c5a3c5f755b3397f
UtsavUpadhyay08 <utsavup2004@gmail.com>
2024-08-27 19:22:06 +0530

    Login successfully implemented

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const puppeteer = require("puppeteer");
 const browserOpenPromise = puppeteer.launch({
     headless: false,
-    slowMo: 50,
+    slowMo: 25,
     defaultViewport: null
 });
 
-const SITE_LINK="https://www.hackerrank.com"
+const SITE_LINK="https://www.hackerrank.com/auth/login"
+const email = process.env.USER_EMAIL;
+const password = process.env.USER_PASSWORD;
 let page;
 
 browserOpenPromise.then(function (browserContext) {
@@ -15,5 +17,20 @@ browserOpenPromise.then(function (browserContext) {
     .then(function (newTab) {
         page = newTab;
         return page.goto(SITE_LINK);
+    })
+    .then(function () {
+        return page.waitForSelector("input[type='text']");
+    })
+    .then(function () {
+        return page.type("input[type='text']", "yidoton361@kwalah.com");
+    })
+    .then(function () {
+        return page.waitForSelector("input[type='password']");
+    })
+    .then(function () {
+        return page.type("input[type='password']", "yidoton361@kwalah.com");
+    })
+    .then(function () {
+        return page.click('button[data-hr-focus-item="private"]');
     })
     .catch((err) => console.log(err));
